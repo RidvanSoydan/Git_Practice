17becbb1bc36b24329e026cf0caa219162f0d918
Rıdvan Soydan <ridvansoydan0711@gmail.com>
2022-12-08 21:01:28 +0300

    Last Commit

---
+++ src/practice1/Method.java
@@ -0,0 +1,10 @@
+package practice1;
+
+public class Method {
+
+    public static void main(String[] args) {
+
+        System.out.println("Method");
+
+    }
+}
---
+++ src/prc2/Inheritance.java
@@ -0,0 +1,10 @@
+package prc2;
+
+public class Inheritance {
+
+    public static void main(String[] args) {
+
+
+        System.out.println("Inheritance");
+    }
+}
