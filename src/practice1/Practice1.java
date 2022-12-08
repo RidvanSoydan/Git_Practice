e236c7516e5a91c7964be46a742d23e9f523436e
Rıdvan Soydan <ridvansoydan0711@gmail.com>
2022-12-08 10:15:42 +0300

    Third Commit

--- src/practice1/Practice1.java
+++ src/practice1/Practice1.java
@@ -3,5 +3,9 @@ package practice1;
 public class Practice1 {
     public static void main(String[] args) {
         System.out.println("Git begins");
+
+        System.out.println("Git continue");
     }
+
+
 }
